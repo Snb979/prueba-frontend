33852e5653eaa873e9cf1ffca10cb1867af5b8a5
Snb979 <130475003+Snb979@users.noreply.github.com>
2026-06-01 06:33:33 -0500

    variables de entorno

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { authStore } from "../store/authStore";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
